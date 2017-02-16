12688c2dd6ac96eb405b8760b4f9933ac412e6ef
Jonathan Goncalves <jonathan@rhumbix.com>
2017-02-16 14:19:24 -0800

    Attempting to commit the statics for now

---
+++ portal/app/staticfiles/js/demo-main.js
@@ -0,0 +1,10 @@
+var demo = demo || {};
+
+$(document).ready(function(ns) {
+    var onLoad = function() {
+        // init fullpage
+        $('#fullpage').fullpage();
+
+    };
+    onLoad();
+}(demo));
