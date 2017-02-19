fbc33ccfb5a81c97a2b808b2da66d2fe7bb2dd1b
Ed Ng <ed.jk.ng@gmail.com>
2017-02-18 18:11:40 -0800

    Linear Regression Saves Model

--- cloud-coordinator/forecasting/linear/linear_regression.py
+++ cloud-coordinator/forecasting/linear/linear_regression.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from sklearn import linear_model
+from sklearn.externals import joblib
 
 class LinearRegression:
     def __init__(self):
@@ -10,8 +11,13 @@ class LinearRegression:
         X, Y = data
         self.model.fit(X, Y)
 
+        joblib.dump(self.model, 'linear_regression.pkl')
+
     def test(self, data):
         X, Y = data
+
+        self.model = joblib.load('linear_regression.pkl')
+
         prediction = self.model.predict(X)
         squared_error = (prediction - Y) ** 2
 
