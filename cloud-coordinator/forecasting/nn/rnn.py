c9a81ce9eef83f8efff65894b36cb72e59db11e8
Ed Ng <ed.jk.ng@gmail.com>
2017-03-08 18:18:17 -0800

    Preliminary RNN Implementation

--- cloud-coordinator/forecasting/nn/rnn.py
+++ cloud-coordinator/forecasting/nn/rnn.py
@@ -1,52 +1,101 @@
 #!/usr/bin/env python
-"""LSTM Neural Network using Kera"""
+"""FeedForward network Developed using Tensorflow"""
 
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers.recurrent import LSTM
-
+import math
 import numpy as np
+import os
+import random
+import time
+import tensorflow as tf
 
 __author__ = "Edward Ng"
 __email__ = "edjng@stanford.edu"
 
-class Recurrent:
-  def __init__(self, num_layer=0, num_neuron=300):
-    self.num_layer = num_layer
-    self.num_neuron = num_neuron
+def generate_batch(data, batch_size=100):
+    X, Y = data
+    indices = np.random.choice(len(X), batch_size, replace=False)
+    return X[indices], Y[indices]
 
-    self.input_size=409
-    self.output_size=24
+class GRU:
+    def __init__(self, depth=24):
+        self.sess = tf.InteractiveSession()
+        self.depth = depth
 
-    self.construct_graph()
+        self.batch_size = 100
+        self.output_size = 24
+        self.max_time = 24
+        self.unit_size = 2
 
-  def construct_graph(self):
-    self.model = Sequential()
+        self.model_name = "./saved_models/gru.ckpt"
+        self.construct_graph()
 
-    if self.num_layer == 0:
-      self.model.add(LSTM(self.output_size, input_dim=1))
-    else:
-      self.model.add(LSTM(self.num_neuron, input_dim=1))
+    # def layer(self, X, num_neuron):
+    #     input_shape = X.get_shape()
 
-      for _ in xrange(self.num_layer - 1):
-        self.model.add(Dense(self.num_neuron))
+    #     #layer takes input X, with num_neuron many neurons. Relu is used for activation.
+    #     W = tf.get_variable("W", shape=[input_shape[1], num_neuron], initializer=tf.contrib.layers.xavier_initializer())
+    #     b = tf.get_variable("b", shape=[num_neuron], initializer=tf.constant_initializer(0))
+    #     Y = tf.matmul(X, W) + b
+    #     return tf.nn.relu(Y)
 
-      # projection layer
-      self.model.add(Dense(self.output_size))
+    def construct_graph(self):
+        self.x = tf.placeholder(tf.float32, [None, self.max_time, self.depth])
+        self.y_ = tf.placeholder(tf.float32, [None, self.output_size])
 
+        cell = tf.contrib.rnn.GRUCell(2, self.input_size)
+        state = tf.Variable(cell.zero_states(self.unit_size, tf.float32), trainable=False)
 
-    self.model.compile(loss='mean_squared_error', optimizer='adam')
+        rnn_outputs, rnn_states = tf.nn.dynamic_rnn(cell, self.x, initial_state=initial_state)
 
-  def train(self, data):
-    x, y = data
-    history = self.model.fit(x, y, validation_split=0.1, nb_epoch=0, batch_size=100, verbose=1)
-    self.model.save_weights("./model.lstm.ckpt")
-    return history
+        #add projection layer
+        with tf.variable_scope("proj_layer"):
+            hidden_shape = rnn_outputs.get_shape()
+            W = tf.get_variable("W", shape=[hidden_shape[1], self.output_size],
+                initializer=tf.contrib.layers.xavier_initializer())
 
-  def test(self, data):
-    x, y = data
-    self.model.load_weights("./model.lstm.ckpt")
+            b = tf.get_variable("b", shape=[self.output_size], initializer=tf.constant_initializer(0))
+            y = tf.matmul(h, W) + b
 
-    y_ = self.model.predict(x)
+            self.prediction = y
 
-    return np.mean(np.square(y - y_)) / np.mean(y)
+            self.squared_error = tf.reduce_mean(tf.square(self.prediction - self.y_))
+            tf.summary.scalar('mean_squared_error', self.squared_error)
+
+            self.train_step = tf.train.RMSPropOptimizer(1e-4) \
+                .minimize(self.squared_error)
+            self.saver = tf.train.Saver()
+
+    def train(self, data):
+        t0 = time.clock()
+        x, y = data
+
+        print '{}: training with data: ({})'.format(type(self).__name__, x.shape)
+
+        merged = tf.summary.merge_all()
+        writer = tf.summary.FileWriter('./summary', graph=tf.get_default_graph())
+
+        tf.global_variables_initializer().run()
+
+        for i in range(2000):
+            batch_xs_train, batch_ys_train = generate_batch(data, self.batch_size)
+            batch_ys_train.reshape(self.batch_size, 24)
+
+            summary, error, _ = self.sess.run([merged, self.squared_error, self.train_step],
+                feed_dict={
+                self.x: batch_xs_train,
+                self.y_: batch_ys_train
+                })
+            writer.add_summary(summary, i)
+
+            if i % 50 == 0:
+                print 'iteration ({}): ({}) squared error'.format(i, error)
+
+        save_path = self.saver.save(self.sess, self.model_name)
+        print '{}: {} seconds took for training'.format(type(self).__name__, time.clock() - t0)
+
+    def predict(self, data):
+        x, y = data
+
+        print '{}: testing with data: ({})'.format(type(self).__name__, x.shape)
+        self.saver.restore(self.sess, self.model_name)
+        return self.sess.run(self.prediction, feed_dict = {self.x: x, self.y_: y})
