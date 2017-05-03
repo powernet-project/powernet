be7e8f433c78b19280d566f291fd978a24d477cd
Jonathan Goncalves <jonathan@rhumbix.com>
2017-05-03 15:03:21 -0700

    Some pubsub tweaks

--- pubsub-service/README.md
+++ pubsub-service/README.md
@@ -1,3 +1,24 @@
 # Publish / Subscribe Micro-Service
 
 - Handles messaging from one or more Home Hubs
+
+## Setup and running the project locally
+
+- After you pull the project down (at the top powernet level), cd into this dir
+```
+cd pubsub-service
+```
+
+- Then run the following setup commands:
+```
+npm install
+export PUBSUB_TOPIC=home-hub-message
+export PUBSUB_SUBSCRIPTION=home-hub-sub-update
+```
+
+- Lastly, run the node server:
+```
+npm start
+```
+
+- Now you can launch a browser and hit localhost:8080 to see the app running
--- pubsub-service/app.js
+++ pubsub-service/app.js
@@ -1,4 +1,3 @@
-// [START app]
 'use strict';
 
 const express = require('express');
@@ -13,65 +12,73 @@ const PubSub = require('@google-cloud/pubsub');
 
 // Instantiate a pubsub client
 const pubsub = PubSub();
-
 const app = express();
-app.set('view engine', 'pug');
-
 const formBodyParser = bodyParser.urlencoded({ extended: false });
 const jsonBodyParser = bodyParser.json();
-
-// List of all messages received by this instance
-const messages = [];
+const messages = []; // List of all messages received by this instance
 
 // The following environment variables are set by app.yaml when running on GAE,
 // but will need to be manually set when running locally.
 const PUBSUB_VERIFICATION_TOKEN = process.env.PUBSUB_VERIFICATION_TOKEN;
-
 const topic = pubsub.topic(process.env.PUBSUB_TOPIC);
+const subscription = pubsub.subscription(process.env.PUBSUB_SUBSCRIPTION);
 
-// [START index]
+// assign the view engine we're going to use
+app.set('view engine', 'pug');
+
+// setup the index route  
 app.get('/', (req, res) => {
-  res.render('index', { messages: messages });
+	res.render('index');
 });
 
 app.post('/', formBodyParser, (req, res, next) => {
-  if (!req.body.payload) {
-    res.status(400).send('Missing payload');
-    return;
-  }
+	if (!req.body.payload) {
+    	res.status(400).send('Missing payload');
+    	return;
+  	}
 
-  topic.publish({
-    data: req.body.payload
-  }, (err) => {
-    if (err) {
-      next(err);
-      return;
-    }
-    res.status(200).send('Message sent');
-  });
+	topic.publish({data: req.body.payload}, (err) => {
+		if (err) {
+			next(err);
+			return;
+		}
+		res.status(200).send('Message sent');
+	});
 });
-// [END index]
 
-// [START push]
-app.post('/pubsub/push', jsonBodyParser, (req, res) => {
-  if (req.query.token !== PUBSUB_VERIFICATION_TOKEN) {
-    res.status(400).send();
-    return;
-  }
+// pull sub message setup
+app.get('/messages', (req, res) => {
+	messages.push("Hellloooo, I'm alive")
 
-  // The message is a unicode string encoded in base64.
-  const message = new Buffer(req.body.message.data, 'base64').toString('utf-8');
+	subscription.pull().then((results) => {
+		const data = results[0];
+		data.forEach((message) => {
+			messages.push(`* %d %j %j`, message.id, message.data, message.attributes);
+		});
+	});
 
-  messages.push(message);
-
-  res.status(200).send();
+	res.render('messages', { messages: messages });
 });
-// [END push]
+
+// push sub message setup
+// app.post('/pubsub/push', jsonBodyParser, (req, res) => {
+//   if (req.query.token !== PUBSUB_VERIFICATION_TOKEN) {
+//     res.status(400).send();
+//     return;
+//   }
+
+//   // The message is a unicode string encoded in base64.
+//   const message = new Buffer(req.body.message.data, 'base64').toString('utf-8');
+
+//   messages.push(message);
+
+//   res.status(200).send();
+// });
+
 
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
+  	console.log(`App listening on port ${PORT}`);
+  	console.log('Press Ctrl+C to quit.');
 });
-// [END app]
