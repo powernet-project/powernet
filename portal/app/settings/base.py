530263343805e10b31b68780fb66fca8769a11a8
Jonathan Goncalves <jonathan@rhumbix.com>
2017-05-24 22:45:50 -0500

    Fixing env vars

--- portal/app/settings/base.py
+++ portal/app/settings/base.py
@@ -22,11 +22,11 @@ INTERNAL_IPS = ('192.168.134.1', '127.0.0.1', '0.0.0.0')
 SECRET_KEY = 's$#@4*=sz+tvjhcufeijpch-&9&gseo1hn1(vbv+0=8_l+8+_p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', True)        # FIXME: pull from envar
+DEBUG = (os.environ.get('DEBUG', 'True').lower() == 'true')
 ALLOWED_HOSTS = [
     'localhost',  # just so we can run debug false locally if needed
     '127.0.0.1',
-]  # FIXME: update to appropriate domain
+]
 ROOT_URLCONF = 'app.urls'
 WSGI_APPLICATION = 'app.wsgi.application'
 
