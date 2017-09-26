3204a4ef364648fe238976a70dcb2c2455b5e0fd
Jonathan Goncalves <jonathan@rhumbix.com>
2017-09-26 08:15:36 -0700

    Build is breaking on gcp

--- portal/app/settings/base.py
+++ portal/app/settings/base.py
@@ -22,13 +22,13 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Application definition
 INSTALLED_APPS = [
-    'app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE_CLASSES = [
