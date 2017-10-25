4e9bc71eefa7edc8fa0a17d8cd02ab094f166340
Jonathan Goncalves <jonathan@rhumbix.com>
2017-10-24 21:18:38 -0700

    Added in svg mimetype support....maybe

--- portal/app/settings/base.py
+++ portal/app/settings/base.py
@@ -1,4 +1,8 @@
 import os
+import mimetypes
+
+mimetypes.add_type("image/svg+xml", ".svg", True)
+mimetypes.add_type("image/svg+xml", ".svgz", True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
