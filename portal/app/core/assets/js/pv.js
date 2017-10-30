2ff51e9fafd46a79f617b0b78f363aaeb70ce9fc
Jonathan Goncalves <jonathan@rhumbix.com>
2017-10-30 16:32:17 -0700

    Added in solaredge api query

--- portal/app/core/assets/js/pv.js
+++ portal/app/core/assets/js/pv.js
@@ -4,6 +4,7 @@ $(document).ready(function(ns) {
     var onLoad = function() {
         // setup listeners
         loadPvStatus();
+        loadStorageStatus();
     },
 
     loadPvStatus = function() {
@@ -13,6 +14,28 @@ $(document).ready(function(ns) {
 
         $.get(enphaseUrl + queryParams + apiKeyPanelOne, function(data) {
             $('#pv-status-home-one').html(JSON.stringify(data, null, '\t'));
+        });
+    },
+
+    loadStorageStatus = function() {
+        var storageUrl = 'https://monitoringapi.solaredge.com/sites/list',
+            queryParams = '?size=5&searchText=Lyon&sortProperty=name&sortOrder=ASC',
+            apiKeyStorageOne = '&api_key=UZCVK3JK5KNY1LXY8F2ZCTEWUK7X4CN5';
+
+        $.ajax({
+            url: storageUrl + queryParams + apiKeyStorageOne,
+            type: 'GET',
+            crossDomain: true,
+            dataType: 'jsonp',
+            success: function(data) {
+                alert("Success");
+                $('#storage-status-home-one').html(JSON.stringify(data, null, '\t'));
+            },
+            error: function() { alert('Failed!'); }
+        });
+
+        $.get(storageUrl + queryParams + apiKeyStorageOne, function(data) {
+
         });
     };
 
