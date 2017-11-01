4cae3d7316d234a9c12914ddee8abf71c8d0547a
Jonathan Goncalves <jonathan@rhumbix.com>
2017-11-01 06:03:56 -0700

    Updated and modularized the demos

---
+++ portal/app/core/assets/js/demo2.js
@@ -0,0 +1,9 @@
+var demo2 = demo2 || {};
+
+$(document).ready(function(ns) {
+    var onLoad = function() {
+        console.warn('wtf');
+    };
+
+    onLoad();
+}(demo2));
---
+++ portal/app/core/assets/js/demo3.js
@@ -0,0 +1,9 @@
+var demo3 = demo3 || {};
+
+$(document).ready(function(ns) {
+    var onLoad = function() {
+        console.warn('wtf');
+    };
+
+    onLoad();
+}(demo3));
