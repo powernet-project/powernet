ab6a602c1b20b4aca48070ccd46817e8adb6266e
Jonathan Goncalves <jonathan@rhumbix.com>
2017-10-31 21:47:39 -0700

    Adding in charts and the new endpoint for power consumption

--- portal/app/api/v1/endpoint/appliance_data.py
+++ portal/app/api/v1/endpoint/appliance_data.py
@@ -1,6 +1,7 @@
 from app.models import ApplianceJsonData
+from rest_framework.response import Response
 from rest_framework.decorators import list_route
-from rest_framework import (viewsets, serializers)
+from rest_framework import (viewsets, serializers, status)
 
 
 class ApplianceJsonDataSerializer(serializers.ModelSerializer):
@@ -16,4 +17,15 @@ class ApplianceJsonDataViewSet(viewsets.ModelViewSet):
     # build a list route that takes an id and returns a serialized consumption for the device
     @list_route(methods=['GET'])
     def consumption(self, request):
-        pass
+        id = request.query_params.get('id')
+        rms_value = ApplianceJsonData.objects.filter(devices_json__contains=[{'sensor_id': int(id)}]).order_by('-id')
+
+        if rms_value.count() == 0:
+            return Response({'result': 'no data for the supplied id'}, status=status.HTTP_404_NOT_FOUND)
+
+        for item in rms_value[0].devices_json:
+            if item['sensor_id'] == int(id):
+                result = item['samples'][0]['RMS']
+                break
+
+        return Response({'result': result}, status=status.HTTP_200_OK)
