32a2a5ad90fa5ba8ed5ebb3685f5d2bc15080c29
Gustavo Vianna Cezar <gcezar@stanford.edu>
2018-07-09 17:17:16 -0700

    database code finished

--- home-hub/communications/database/sqliteDB_Routine.py
+++ home-hub/communications/database/sqliteDB_Routine.py
@@ -21,14 +21,16 @@ class HardwareRPi:
         self.REQUEST_TIMEOUT = 10
         self.PWRNET_API_BASE_URL = 'http://pwrnet-158117.appspot.com/api/v1/'
         self.SENTRY_DSN = 'https://e3b3b7139bc64177b9694b836c1c5bd6:fbd8d4def9db41d0abe885a35f034118@sentry.io/230474'
-        self.app_orig_states = ["OFF", "OFF", "ON", "OFF", "OFF", "OFF"] # Battery not included
-        self.app_new_status = ["OFF", "OFF", "ON", "OFF", "OFF", "OFF"]  # Battery not included
-        # Sentry setup for additional error reporting via 3rd party cloud service
-        # self.client = Client(self.SENTRY_DSN)
+        self.app_orig_states = ["OFF", "OFF", "ON", "OFF", "OFF", "OFF","OFF"] # Battery not included
+        self.app_new_status = ["OFF", "OFF", "ON", "OFF", "OFF", "OFF", "OFF"]  # Battery not included
 
         # Initializing GPIOs:
         GPIO.setmode(GPIO.BOARD)
-        self.appliance_lst = ["AC1", "SE1", "RF1", "CW1", "DW1"]
+        # Dictionary with db states id:
+        # Format: {'appliance_name': ['local_DB_id', 'pwrnet_appspot_id'], ...}
+        self.input_sources_statesDB = {'AC1': [3,22], 'SE1': [4,23], 'RF1':[6,25], 'DW1':[7,28],'WM1':[8,29], 'CW1': [9,24], 'PW2':[10,25]}
+        self.sourcesDBID = [self.input_sources_statesDB['AC1'][0],self.input_sources_statesDB['SE1'][0],self.input_sources_statesDB['RF1'][0],self.input_sources_statesDB['CW1'][0],self.input_sources_statesDB['DW1'][0],self.input_sources_statesDB['WM1'][0],self.input_sources_statesDB['PW2'][0]]
+        self.appliance_lst = ["AC1", "SE1", "RF1", "CW1", "DW1", "WM1"]
 
         self.N_SAMPLES = N_SAMPLES
         self.adc_Vin = adc_Vin
@@ -37,6 +39,8 @@ class HardwareRPi:
         # delta in power to save in db
         self.dP = 0.3
         self.flag_db = 0
+        self.flag_state = 0
+
 
         # Initializing SPI
         self.spi = spidev.SpiDev()
@@ -95,19 +99,35 @@ class HardwareRPi:
 
 
     def dbWriteMeasurements(self, vals):
-
       try:
           #print 'connecting'
           conn = sqlite3.connect('homehubDB.db')
           c = conn.cursor()
-          print vals
-          c.execute("INSERT INTO measurements VALUES ((?), (?), (?), (?),)",.\
-          (vals[0], vals[1], vals[2], vals[3]))
+          #print vals
+          c.execute("INSERT INTO measurements (rms, currentdate, currenttime, source_id) VALUES ((?), (?), (?), (?))" , (vals[0], vals[1], vals[2], vals[3]))
       except sqlite3.IntegrityError:
           print 'error connecting to db'
           #print('ERROR: ID already exists in PRIMARY KEY column {}'.format(id_column))
       conn.commit()
       conn.close()
+
+
+    def dbWriteStates(self, vals):
+      try:
+          #print 'connecting'
+          conn = sqlite3.connect('homehubDB.db')
+          c = conn.cursor()
+          #print vals
+          c.execute("INSERT INTO input_sources_state (state, currentdate, currenttime, source_id) VALUES ((?), (?), (?), (?))" , (vals[0], vals[1], vals[2], vals[3]))
+          #print 'State written...'
+          #print 'Vals: ', vals
+      except sqlite3.IntegrityError:
+          print 'error connecting to db'
+          #print('ERROR: ID already exists in PRIMARY KEY column {}'.format(id_column))
+      conn.commit()
+      conn.close()
+
+
 
           #c.execute("""INSERT INTO measurements (rms,
             #  currentdate, currenttime, source_id) VALUES((?), (?),
@@ -119,8 +139,15 @@ class HardwareRPi:
 
 if __name__ == '__main__':
     test = HardwareRPi()
-    prev = [-1,-1,-1,-1,-1,-1,-1,-1]
+
+    status_PW2 = "OFF"
+    power_PW2 = 0.0
+    cosphi_PW2 = 1.0
+
+    #prev = [-1,-1,-1,-1,-1,-1,-1,-1]
     while True:
+        '''
+
         dts = []  # date/time stamp for each start of analog read
         #AC id:5
         dts.append(str(datetime.now()))
@@ -152,26 +179,60 @@ if __name__ == '__main__':
         #print temp_ai[0]
 
         data = test.RMS(temp_ai)
-
         # i+1 is foreign key for db
         for i in range(len(data)):
             if data[i] >= prev[i]+test.dP or data[i] <= prev[i]-test.dP:
                 temp = [data[i], dts[i].split()[0], dts[i].split()[1], i+1]
-                test.dbWriteMeasurements(temp)
-                test.flag_db = 1
+                try:
+                    test.dbWriteMeasurements(temp)
+                    test.flag_db = 1
+                except Exception as exc:
+                    #self.logger.exception(exc)
+                    #client.captureException()
+                    pass
+
         if test.flag_db == 1:
             prev = copy.deepcopy(data)
             test.flag_db = 0
+        '''
 
 
-    	#print data
-    	print data[3]
-        # connecting to sqlite3
-        #conn = sqlite3.connect('homehubDB.db')
+        # date/time stamp for device status
 
-        vals = [data[3], str(datetime.today()).split()[0], str(datetime.today()).split()[1], 1]
-        #test.dbWrite('measurements', vals)
-        print vals
+
+        dev_status = requests.get(test.PWRNET_API_BASE_URL + "device", timeout=test.REQUEST_TIMEOUT).json()["results"]
+        dts = str(datetime.now())
+        status_AC1 = [v for v in dev_status if v['id']==test.input_sources_statesDB['AC1'][1]][0]['status']
+        status_SE1 = [v for v in dev_status if v['id']==test.input_sources_statesDB['SE1'][1]][0]['status']
+        status_RF1 = [v for v in dev_status if v['id']==test.input_sources_statesDB['RF1'][1]][0]['status']
+        status_CW1 = [v for v in dev_status if v['id']==test.input_sources_statesDB['CW1'][1]][0]['status']
+        status_DW1 = [v for v in dev_status if v['id']==test.input_sources_statesDB['DW1'][1]][0]['status']
+        status_WM1 = [v for v in dev_status if v['id']==test.input_sources_statesDB['WM1'][1]][0]['status']
+        # Battery
+        status_PW2 = [v for v in dev_status if v['id']==test.input_sources_statesDB['PW2'][1]][0]['status']
+        power_PW2 = [v for v in dev_status if v['id']==test.input_sources_statesDB['PW2'][1]][0]['value']
+        cosphi_PW2 = [v for v in dev_status if v['id']==test.input_sources_statesDB['PW2'][1]][0]['cosphi']
+
+        test.app_new_status = [status_AC1, status_SE1, status_RF1, status_CW1, status_DW1, status_WM1]
+
+        for i in range(len(test.app_new_status)):
+            if test.app_new_status[i] != test.app_orig_states[i]:
+                temp_db = [test.app_new_status[i], dts.split()[0], dts.split()[1], test.sourcesDBID[i]]
+                try:
+                    # devices_act(test.appliance_lst[i], test.app_new_status[i])
+                    test.dbWriteStates(temp_db)
+                    test.flag_state = 1
+                    #print 'try flag: ', test.flag_state
+                except Exception as exc:
+                    test.logger.exception(exc)
+                    client.captureException()
+        if test.flag_state == 1:
+            #print 'app_orig_states: ', test.app_orig_states
+            test.app_orig_states = copy.deepcopy(test.app_new_status)
+            #print 'app_orig_states_new: ', test.app_orig_states
+            test.flag_state = 0
+
+
 
         time.sleep(2)
 
