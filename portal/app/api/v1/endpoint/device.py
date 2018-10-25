53e12603810776b8c3081b7ca97ab54973754bd1
Jonathan Goncalves <jonathan@rhumbix.com>
2018-10-25 11:00:50 -0700

    Add in the post save signal to the Device model

--- portal/app/api/v1/endpoint/device.py
+++ portal/app/api/v1/endpoint/device.py
@@ -3,6 +3,8 @@ from rest_framework.authentication import BasicAuthentication
 from app.api.v1 import CsrfExemptAuth
 from app.common.enum_field_handler import EnumFieldSerializerMixin
 from app.models import Device, DeviceState
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class DeviceSerializer(EnumFieldSerializerMixin, serializers.ModelSerializer):
@@ -27,3 +29,12 @@ class DeviceStateViewSet(viewsets.ModelViewSet):
     authentication_classes = (CsrfExemptAuth.CsrfExemptSessionAuthentication, BasicAuthentication)
     serializer_class = DeviceStateSerializer
     queryset = DeviceState.objects.all()
+
+
+@receiver(post_save, sender=Device)
+def publish_device_change(sender, **kwargs):
+    obj = kwargs.get('instance')
+    print(kwargs.get('created', False))
+    print("Sometimes I wonder what goes on")
+    obj_ser = DeviceSerializer(obj)
+    print(obj_ser)
