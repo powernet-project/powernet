fc4e6f5bb03a0d6bb7d7cd4df4b60a210b7c7b1b
Jonathan Goncalves <jonathan@rhumbix.com>
2018-10-25 11:16:04 -0700

    Got the formatting correct finally - now lets see if it works on the server

--- portal/app/api/v1/endpoint/device.py
+++ portal/app/api/v1/endpoint/device.py
@@ -5,6 +5,8 @@ from app.common.enum_field_handler import EnumFieldSerializerMixin
 from app.models import Device, DeviceState
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from google.cloud import pubsub_v1
+from rest_framework.renderers import JSONRenderer
 
 
 class DeviceSerializer(EnumFieldSerializerMixin, serializers.ModelSerializer):
@@ -33,8 +35,15 @@ class DeviceStateViewSet(viewsets.ModelViewSet):
 
 @receiver(post_save, sender=Device)
 def publish_device_change(sender, **kwargs):
+    # get the object generating the signal and serialize it for transport
     obj = kwargs.get('instance')
-    print(kwargs.get('created', False))
-    print("Sometimes I wonder what goes on")
-    obj_ser = DeviceSerializer(obj)
-    print(obj_ser)
+    obj_ser = JSONRenderer().render(DeviceSerializer(obj).data)
+
+    # init the publisher with our project params - will need HH ref in the future so we can discriminate
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path('pwrnet-158117', 'home-hub-message')
+
+    # publish the message
+    future = publisher.publish(topic_path, data=obj_ser.encode('utf-8'))
+    print('Published {} of message ID {}.'.format(obj_ser.data, future.result()))
+
