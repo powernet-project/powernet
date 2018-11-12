a8f7d3e720b514f4f8a0937a2f0b29117f1227df
Jonathan Goncalves <jonathan@rhumbix.com>
2018-11-12 13:25:11 -0800

    Make sure to properly escape your quotes

--- home-hub/scripts/cmds.sh
+++ home-hub/scripts/cmds.sh
@@ -3,7 +3,8 @@
 cd ~/
 
 echo "Writing aliases to your .bashrc file..."
-echo alias ll='ls -la' >> .bashrc
-echo alias gs='git status' >> .bashrc
-echo alias gpr='git pull --rebase' >> .bashrc
-echo "Done writing aliases to your .bashrc file."
+echo alias ll="'ls -la'" >> .bashrc
+echo alias gs="'git status'" >> .bashrc
+echo alias gpr="'git pull --rebase'" >> .bashrc
+echo "Done writing aliases to your .bashrc file. Reloading your source..."
+source ~/.bashrc
