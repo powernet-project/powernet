f904f8ddd7a849aa560c1055cc24b7bfa60afb83
Jonathan Goncalves <jonathan@rhumbix.com>
2018-11-21 13:10:35 -0800

    [skip ci] logger fix on the network class 3

--- home-hub/main/api_network.py
+++ home-hub/main/api_network.py
@@ -26,7 +26,7 @@ class NetworkInterface:
     def save_rms(self, json_data):
         r = requests.post(self.pwrnet_base_url + 'rms/', json=json_data, timeout=self.request_timeout, headers=self.headers)
         if r.status_code != 201:
-            self.logger.info`('Failed to save rms data')
+            self.logger.info('Failed to save rms data')
             r.raise_for_status()
 
     def save_devices(self, json_data):
