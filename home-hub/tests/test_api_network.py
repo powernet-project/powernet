933b9806935979d9ed0f926e4fcc7b469550bca5
Jonathan Goncalves <jonathan@rhumbix.com>
2018-11-21 07:43:18 -0800

    [skip ci] first test to ensure our api rejects calls with bad auth tokens

--- home-hub/tests/test_api_network.py
+++ home-hub/tests/test_api_network.py
@@ -1,20 +1,16 @@
 import unittest
+from main.api_network import NetworkInterface
+
 
 class TestApiNetwork(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_invalid_token_in_request(self):
+        # initialize the Network API class
+        api = NetworkInterface('FAKE_TOKEN')
+        
+        # ensure that an error is raised when an invalid token is given
+        with self.assertRaises(KeyError):
+            api.get_device_status()
 
 if __name__ == '__main__':
     unittest.main()
