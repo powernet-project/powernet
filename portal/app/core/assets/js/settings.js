445339d4e2d0d1742c58393174850ddc6f37746d
Jonathan Goncalves <jonathan@rhumbix.com>
2019-05-08 10:55:27 -0700

    More nice stuff

--- portal/app/core/assets/js/settings.js
+++ portal/app/core/assets/js/settings.js
@@ -4,9 +4,36 @@ $(document).ready(function(ns) {
     const onLoad = function() {
         $('a .nav-link').removeClass('active');
         $('#nav-link-settings').addClass('active');
+
+        getHomeData();
+        getUserData();
     };
 
+    const getHomeData = () => {
+        $.ajax({
+            url: `/api/v1/home/`,
+            type: 'GET',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', 'Token ' + window.localStorage.getItem('token'));
+            },
+            success: (data) => {
+                $('#home-data').html(JSON.stringify(data, null, '\t'));
+            }
+        });
+    };
 
+    const getUserData = () => {
+        $.ajax({
+            url: `/api/v1/powernet_user/`,
+            type: 'GET',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', 'Token ' + window.localStorage.getItem('token'));
+            },
+            success: (data) => {
+                $('#user-data').html(JSON.stringify(data, null, '\t'));
+            }
+        });
+    };
 
     onLoad();
 }(settings));
