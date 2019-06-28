572a01812b72b7037be926cce263304c09a9823e
Gustavo <gcezar@stanford.edu>
2019-06-28 12:32:58 -0700

    improving code flow

--- portal/app/farm_updater/sonnen_api.py
+++ portal/app/farm_updater/sonnen_api.py
@@ -2,7 +2,6 @@ import requests
 from django.conf import settings
 
 
-
 class SonnenApiInterface:
 
     def __init__(self, url=settings.SONNEN_URL, token=settings.SONNEN_TOKEN):
@@ -10,13 +9,14 @@ class SonnenApiInterface:
         self.token = token
         self.headers = {'Accept': 'application/vnd.sonnenbatterie.api.core.v1+json', 'Authorization': 'Bearer ' +
                                                                                                       self.token}
+        self.status_endpoint = '/api/v1/status'
+        self.control_endpoint = '/api/v1/setpoint/'
 
     def get_batteries_status_json(self, serial):
         # This method does a request to sonnen api to get status
-        status_endpoint = '/api/v1/status'
 
         try:
-            resp = requests.get(self.url + serial + status_endpoint, headers=self.headers)
+            resp = requests.get(self.url + serial + self.status_endpoint, headers=self.headers)
             resp.raise_for_status()
             data = resp.json()
             data['batt_id'] = serial
@@ -27,31 +27,30 @@ class SonnenApiInterface:
             return None
 
     def manual_mode_control(self, serial, mode='charge', value='0'):
-        control_endpoint = '/api/v1/setpoint/'
+
         # Checking if system is in off-grid mode
         voltage = self.get_batteries_status_json().get_status()['Uac']
 
         if voltage == 0:
             print('Battery is in off-grid mode... Cannot execute the command')
-            return {}
+            return None
 
-        else:
-            try:
-                resp = requests.get(self.url + serial + control_endpoint + mode + '/' + value,
-                                    headers=self.headers)
-                resp.raise_for_status()
-
-            except requests.exceptions.HTTPError as err:
-                print(err)
-
+        try:
+            resp = requests.get(self.url + serial + self.control_endpoint + mode + '/' + value,
+                                headers=self.headers)
+            resp.raise_for_status()
             return resp.json()
+
+        except requests.exceptions.HTTPError as err:
+            print(err)
+            return {'Error: ', err}
 
 
 # This method is used on scheduler.py to pull data in given periodicity
 def update_battery_status():
     from app.models import FarmDevice, DeviceType
 
-    devices = FarmDevice.objects.filter(home='1').filter(type=DeviceType.SONNEN).all()
+    devices = FarmDevice.objects.filter(home='1', type=DeviceType.SONNEN)
     sonnen_api = SonnenApiInterface()
 
     for dev in devices:
@@ -72,7 +71,5 @@ def set_battery_action(mode='charge', value='0'):
     sonnen_api = SonnenApiInterface()
     control = sonnen_api.manual_mode_control(mode, value)
     print('control: ', control)
-    if int(control['ResponseCode']) != 0:
+    if control is None or int(control['ResponseCode']) != 0:
         print('Error submitting request to sonnen server: ', control)
-    else:
-        return control
