4fec8dbf575affc1d160dc90904b6b5fea2d5f12
Gustavo <gcezar@stanford.edu>
2019-08-19 09:15:32 -0700

    farm_device_enpoint preliminary

--- portal/app/api/v1/endpoint/farm_device.py
+++ portal/app/api/v1/endpoint/farm_device.py
@@ -2,10 +2,16 @@ from rest_framework import (viewsets, serializers)
 from rest_framework.authentication import TokenAuthentication
 from app.api.v1 import CsrfExemptAuth
 from app.common.enum_field_handler import EnumFieldSerializerMixin
-from app.models import FarmDevice
+from app.models import FarmDevice, FarmData
 
 
 class FarmDeviceSerializer(EnumFieldSerializerMixin, serializers.ModelSerializer):
+    class Meta:
+        model = FarmDevice
+        fields = '__all__'
+
+
+class FarmDeviceDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = FarmDevice
         fields = '__all__'
@@ -19,5 +25,13 @@ class FarmDeviceViewSet(viewsets.ModelViewSet):
         queryset = FarmDevice.objects.filter(home__owner__user=self.request.user).order_by('id')
         return queryset
 
+
+class FarmDeviceDataViewSet(viewsets.ModelViewSet):
+    authentication_classes = (CsrfExemptAuth.CsrfExemptSessionAuthentication, TokenAuthentication)
+    serializer_class = FarmDeviceDataSerializer
+
+    def get_queryset(self, **kwargs):
+        queryset = FarmData.objects.filter(home__owner__user=self.request.user).order_by('id')
+        return queryset
 
 
