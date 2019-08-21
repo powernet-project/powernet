5712e903623db19ba7b7225a525e5749c6beb3eb
Gustavo <gcezar@stanford.edu>
2019-08-21 09:15:12 -0700

    farm_device_enpoint preliminary (#24)
    * farm_device_enpoint preliminary
    
    * fixing errors
    
    * fixing typos
    
    * working code
    
    * testing code for endpoint
    
    * filter by device_id
    
    * including timestamp filter - not finished
    
    * working code
    
    * query for farm_device
    
    * staged changes
    
    * filtering code by device id and date
    
    * including exception
    
    * fixing error code

--- portal/app/api/v1/endpoint/farm_device.py
+++ portal/app/api/v1/endpoint/farm_device.py
@@ -2,12 +2,19 @@ from rest_framework import (viewsets, serializers)
 from rest_framework.authentication import TokenAuthentication
 from app.api.v1 import CsrfExemptAuth
 from app.common.enum_field_handler import EnumFieldSerializerMixin
-from app.models import FarmDevice
+from app.models import FarmDevice, FarmData
+from rest_framework.exceptions import APIException
 
 
 class FarmDeviceSerializer(EnumFieldSerializerMixin, serializers.ModelSerializer):
     class Meta:
         model = FarmDevice
+        fields = '__all__'
+
+
+class FarmDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FarmData
         fields = '__all__'
 
 
@@ -20,4 +27,23 @@ class FarmDeviceViewSet(viewsets.ModelViewSet):
         return queryset
 
 
+class FarmDataViewSet(viewsets.ModelViewSet):
+    serializer_class = FarmDataSerializer
+    authentication_classes = (CsrfExemptAuth.CsrfExemptSessionAuthentication, TokenAuthentication)
 
+    def get_queryset(self, **kwargs):
+        device_uid = self.request.query_params.get('device_uid', None)
+        start_date = self.request.query_params.get('start_date', None)
+        end_date = self.request.query_params.get('end_date', None)
+        if device_uid is None or start_date is None or end_date is None:
+            raise InvalidParamException
+            return
+        queryset = FarmData.objects.filter(farm_device__device_uid=device_uid, timestamp__range=[start_date, end_date])
+        return queryset
+
+
+class InvalidParamException(APIException):
+    status_code = 400
+    default_detail = 'Could not determine an appropriate data set to retrieve, please ensure ' \
+                     'the supplied parameters are correct.'
+    default_code = 'invalid_parameters'
