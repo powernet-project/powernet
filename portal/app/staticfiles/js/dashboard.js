c801f21791cb7bfde8928135a60e3bfae2e2eb07
Jonathan Goncalves <jonathan.goncalves@gmail.com>
2020-02-21 15:02:26 -0800

    add in js

--- portal/app/staticfiles/js/dashboard.js
+++ portal/app/staticfiles/js/dashboard.js
@@ -5,6 +5,46 @@ $(document).ready(function(ns) {
         $('a .nav-link').removeClass('active');
         $('#nav-link-dashboard').addClass('active');
         $('#view-ecobee-chart-btn').on('click', startDataInterval);
+        $('#set-ecobee-temp-mode-params').on('click', setTempMode);
+
+        // create dropdown options for the temperature
+        let drpdTemp = $('#ecobee-temp'),
+            tempOptions = '';
+
+        for(let i = 80; i >= 60; i--) {
+            tempOptions += '<option value="' + (i * 10) + '">' + i + 'ºF</option>'
+        }
+
+        drpdTemp.append(tempOptions);
+    };
+
+    let setTempMode = function() {
+        let selectedTemp = $('#ecobee-temp').val(),
+            selectedMode = $('#ecobee-hvac-mode').val();
+
+        console.log(selectedMode, selectedTemp);
+
+        $.ajax({
+            url: '/api/v1/ecobee/temperature/' + selectedTemp,
+            type: "POST",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', 'Token ' + window.localStorage.getItem('token'));
+            },
+            success: function (data) {
+                console.log('Setting ecobee temp', data);
+            }
+        });
+
+        $.ajax({
+            url: '/api/v1/ecobee/mode/' + selectedMode,
+            type: "POST",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', 'Token ' + window.localStorage.getItem('token'));
+            },
+            success: function (data) {
+                console.log('Setting ecobee mode', data);
+            }
+        });
     };
 
     let startDataInterval = function() {
@@ -18,7 +58,7 @@ $(document).ready(function(ns) {
 
     let requestEcobeeData = function() {
         $.ajax({
-            url: '/api/v1/ecobee_data',
+            url: '/api/v1/ecobee/data',
             type: "GET",
             beforeSend: function (xhr) {
                 xhr.setRequestHeader('Authorization', 'Token ' + window.localStorage.getItem('token'));
@@ -26,15 +66,30 @@ $(document).ready(function(ns) {
             success: function (data) {
                 console.log(data);
                 let actualTemp = data['runtime']['actualTemperature'] / 10,
-                    actualTime = data['thermostatTime'];
-                Plotly.extendTraces('ecobee-chart', {y: [[actualTemp]], x:[[actualTime]]}, [0]);
+                    actualTime = data['thermostatTime'],
+                    hvacMode = data['settings']['hvacMode'];
+                Plotly.extendTraces('ecobee-chart', {y: [[actualTemp], [hvacMode]], x:[[actualTime], [actualTime]]}, [0, 1]);
             }
         });
     };
 
     let displayChart = function() {
-        let plotData = [{ x: [], y: [], type: 'scatter' }];
-        Plotly.newPlot('ecobee-chart', plotData);
+        let temperatureTrace = { x: [], y: [], type: 'scatter', name: 'Temperature' },
+            hvacModeTrace = { x: [],  y: [], type: 'scatter', name: 'HVAC Mode', yaxis: 'y2' },
+            plotData = [temperatureTrace, hvacModeTrace],
+            layout = {
+                title: 'Ecobee Temperature/Mode',
+                yaxis: { title: 'Temp ºF' },
+                yaxis2: {
+                    title: 'HVAC Mode',
+                    titlefont: { color: 'rgb(148, 103, 189)' },
+                    tickfont: { color: 'rgb(148, 103, 189)' },
+                    overlaying: 'y',
+                    side: 'right'
+                }
+            };
+
+        Plotly.newPlot('ecobee-chart', plotData, layout);
     };
 
     onLoad();
