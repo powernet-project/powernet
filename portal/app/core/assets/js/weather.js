cfba87a82da834bec8b7b1e92e34d13e799c533d
Derin Serbetcioglu <serbetd@gmail.com>
2020-03-31 13:58:17 -0700

    weather: weather.js updated and DRY'ed up (POW-2)

--- portal/app/core/assets/js/weather.js
+++ portal/app/core/assets/js/weather.js
@@ -1,9 +1,12 @@
 var weather = weather || {};
+var owmUrl = "https://api.openweathermap.org/data/2.5/weather?"
+var appid = "6faafe3de999509b86b118803ee2ca8f";
 
 $(document).ready(function(ns) {
     var onLoad = function() {
             // setup listeners
             setupListeners();
+            ns.queryWeatherBasedOnCoords();
         },
 
         setupListeners = function() {
@@ -13,31 +16,36 @@ $(document).ready(function(ns) {
         };
 
     ns.queryWeatherBasedOnZip = function(zipCode) {
-        // Query the OWM API for weather data
-        data = JSON.stringify(data, null, '\t');
-        var url = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipCode +
-            ",us&APPID=6faafe3de999509b86b118803ee2ca8f";
-        $.get(url, function(data) {
-            $("#weather-response").html(data);
+        // Query the OWM API for weather data by zip code
+        ns.owmApiCall(zipCode, null, null).then(function(data){
+            $("#weather-response").html(JSON.stringify(data, null, '\t'));
         });
+    };
+
+    ns.queryWeatherBasedOnCoords = function() {
+        // Query the OWM API for weather data by geolocation
+        navigator.geolocation.getCurrentPosition(function(position) {
+            let latitude = position.coords.latitude;
+            let longitude = position.coords.longitude;
+            ns.owmApiCall(null, latitude, longitude).then(function(data) {
+                let urlIcon = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+                let temp = data.main.temp;
+                $("#temp").html(temp + "° F");
+                $("#icon").html("<img src=" + urlIcon +  ">");
+            });
+        });
+    };
+
+    ns.owmApiCall = function(zipcode, latitude , longitude) {
+        // OWM API call
+        let url = owmUrl + "APPID=" + appid;
+        if (zipcode) {
+            url += "&zip=" + zipcode;
+        } else {
+            url += "&lat=" + latitude + "&lon=" + longitude + "&units=imperial";
+        }
+        return $.get(url);
     };
 
     onLoad();
 }(weather));
-
-
-// weather functionality for all pages
-navigator.geolocation.getCurrentPosition(function(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-    var urlPosition = "https://api.openweathermap.org/data/2.5/weather?lat=" +
-        latitude + "&lon=" + longitude + "&APPID=6faafe3de999509b86b118803ee2ca8f";
-
-    $.getJSON(urlPosition, function(data) {
-        urlIcon = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
-        // kelvin to fahrenheit conversion
-        temp = Math.round((data.main.temp - 273.15) * 1.8 + 32);
-        $("#temp").html(temp + "° F");
-        $("#icon").html("<img src=" + urlIcon +  ">");
-    });
-});
