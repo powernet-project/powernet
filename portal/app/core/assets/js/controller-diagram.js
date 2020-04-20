41fab0fc57aaf0a7ee383cb25486adc12b82be3e
Derin Serbetcioglu <serbetd@gmail.com>
2020-04-20 14:11:24 -0700

    small clean up

--- portal/app/core/assets/js/controller-diagram.js
+++ portal/app/core/assets/js/controller-diagram.js
@@ -7,12 +7,12 @@ var diagram = new SimpleDiagram('#diagram', {
 
 var nodes = [
     {name: 'A', row: 5, column: 1, connectsTo: 'D', shape: 'grid', source: './'},
-    {name: 'B', row: 1, column: 9, connectsTo: 'D', shape: 'solar_panel', source: './', color: 'reds'},
-    {name: 'C', row: 3, column: 10, connectsTo: 'D', shape: 'washer', source: './', color: 'reds'},
-    {name: 'D', row: 5, column: 4, shape: 'house', source: './', color: 'reds'},
-    {name: 'E', row: 7, column: 10, connectsTo: 'D', shape: 'battery', source: './', color: 'reds'},
-    {name: 'F', row: 9, column: 9, connectsTo: 'D', shape: 'charger', source: './', color: 'reds'},
-    {name: 'G', row: 5, column: 11, connectsTo: 'D', shape: 'hvac', source: './', color: 'reds'},
+    {name: 'B', row: 1, column: 9, connectsTo: 'D', shape: 'solar_panel', source: './', color: 'red'},
+    {name: 'C', row: 3, column: 10, connectsTo: 'D', shape: 'washer', source: './'},
+    {name: 'D', row: 5, column: 4, shape: 'house', source: './'},
+    {name: 'E', row: 7, column: 10, connectsTo: 'D', shape: 'battery', source: './'},
+    {name: 'F', row: 9, column: 9, connectsTo: 'D', shape: 'charger', source: './'},
+    {name: 'G', row: 5, column: 11, connectsTo: 'D', shape: 'hvac', source: './'},
 ];
 
 // Draw the nodes!
