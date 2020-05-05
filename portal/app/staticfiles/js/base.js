bc9128a3a6599134bd45c9364b7abe6c46307fe7
Jonathan Goncalves <jonathan.goncalves@gmail.com>
2020-05-04 21:25:46 -0700

    update requirements and statics

---
+++ portal/app/staticfiles/js/base.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	let isSidebarExpanded = localStorage.getItem("isSidebarExpanded");
+	// sidebar expanded by default
+	if (isSidebarExpanded === null) {
+	    // value hasn't been set yet
+	    localStorage.setItem("isSidebarExpanded", "true");
+	}
+	if (isSidebarExpanded === "false") {
+	    // false means not expaded
+	    $('#sidebar').toggleClass('active');
+	}
+});
+
+function toggleSideBar() {
+	$('#sidebar').toggleClass('active');
+	let isSidebarExpanded = localStorage.getItem("isSidebarExpanded") === "true";
+	localStorage.setItem("isSidebarExpanded", (!isSidebarExpanded).toString());
+}
