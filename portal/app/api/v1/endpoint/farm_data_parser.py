1e017b657a81d834416c90bb1a71245f4a7a0a51
Derin Serbetcioglu <serbetd@gmail.com>
2020-08-03 14:34:56 -0700

    values in energy summary page rounded to integer (#65)
    * values in energy summary page rounded to integer
    
    * file structure updated
    
    * improved df_round() function to dynamic

--- portal/app/api/v1/endpoint/farm_data_parser.py
+++ portal/app/api/v1/endpoint/farm_data_parser.py
@@ -2,22 +2,17 @@ import json
 import pandas as pd
 import datetime
 
-def celsius_to_fahr(temp_c):
-    """
-        Convert Celsius to Fahrenheit
-    """
-    temp_f = temp_c * 9 / 5 + 32
 
-    return temp_f
+# Data Processing
 
-def utc_to_pst(time):
+def main_farm_parser(data):
     """
-        Convert TimeZone UTC to Pacific
+        Parses json blob for device_id = 17 and returns processed data
     """
-    time_utc = pd.to_datetime(time)
-    time_pst = time_utc - datetime.timedelta(hours=8)
+    device_data = json.loads(data["device_data"])
+    farm_data = device_data.get("processed")
+    return farm_data
 
-    return time_pst
 
 def energy_summary_parser(argv):
     """
@@ -28,13 +23,15 @@ def energy_summary_parser(argv):
     data = argv[-1]
     # pulls device data from the json
     farm_data = [ json.loads(row.get("device_data")) for row in data ]
-    # creates dataframe with argument fields as columns
 
+    # creates dataframe with argument fields as columns
     farm_df = pd.DataFrame(farm_data)[argv[:-1]]
     farm_df["timestamp"] = utc_to_pst(farm_df["timestamp"])
 
     # average every 10 minute time interval
     farm_df = farm_df.resample('15Min', on="timestamp").mean().reset_index()
+    # round df values
+    farm_df = df_round(farm_df)
 
     # prepares json to be sent to javascript
     return farm_df.to_json(date_format="iso")
@@ -52,21 +49,13 @@ def local_fan_info_parser(data):
     return farm_df.to_json()
 
 
-def main_farm_parser(data):
-    """
-        Parses json blob for device_id = 17 and returns processed data
-    """
-    device_data = json.loads(data["device_data"])
-    farm_data = device_data.get("processed")
-    return farm_data
-
-
 def main_power_parser(data):
     """
         Parses json blob for device_id = 17 and returns processed data
         Converts power from W to kW and adds field energy to the dataframe
     """
     farm_data = []
+    # remove row if value is None
     for row in data:
         row_val = main_farm_parser(row)
         if row_val is not None:
@@ -77,5 +66,38 @@ def main_power_parser(data):
     farm_df["energy"] = farm_df["POWER_TEST_PEN"] / 12
     # timezone changed from utc to pacific
     farm_df["timestamp"] = utc_to_pst(farm_df["timestamp"])
+    # round df values
+    farm_df = df_round(farm_df)
 
     return farm_df.to_json(date_format="iso")
+
+
+# Helper functions
+
+def celsius_to_fahr(temp_c):
+    """
+        Convert Celsius to Fahrenheit
+    """
+    temp_f = temp_c * 9 / 5 + 32
+
+    return temp_f
+
+
+def utc_to_pst(time):
+    """
+        Convert TimeZone UTC to Pacific
+    """
+    time_utc = pd.to_datetime(time)
+    time_pst = time_utc - datetime.timedelta(hours=8)
+
+    return time_pst
+
+
+def df_round(df, columns_to_ignore=['timestamp']):
+    """
+        Rounds the values of a dataframe unless in columns_to_ignore list
+    """
+    for name in df.columns:
+        if name not in columns_to_ignore:
+            df[name] = round(df[name])
+    return df
