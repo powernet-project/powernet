96376d245561d4e31a74307ca5ecb2be20f1ca6f
Derin Serbetcioglu <serbetd@gmail.com>
2021-04-06 14:46:42 -0700

    username updated

--- portal/app/settings/base.py
+++ portal/app/settings/base.py
@@ -97,7 +97,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
-        'USER': 'jongon',
+        'USER': 'powernet-db-user',
         'PASSWORD': DB_PASSWORD,
         'HOST': '/cloudsql/pwrnet-158117:us-central1:pwrnet-dev-store',
         'PORT': '5432'
