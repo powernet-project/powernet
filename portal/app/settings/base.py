4981b3e831a44952af95cfcc41c63cd6af2b7e66
Derin Serbetcioglu <serbetd@gmail.com>
2021-04-09 17:27:03 -0700

    testing still

--- portal/app/settings/base.py
+++ portal/app/settings/base.py
@@ -97,7 +97,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'jongon',
-        'PASSWORD': '',
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
         'HOST': '/cloudsql/pwrnet-158117:us-central1:pwrnet-dev-store',
         'PORT': '5432'
     }
--- portal/manage.py
+++ portal/manage.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings.base")
 
     from django.core.management import execute_from_command_line
-
+    print("env", os.environ.get('DATABASE_PASSWORD'))
     execute_from_command_line(sys.argv)
