f78fb1c3d5d958cc6e1a78e0acdbf41b97dfa06e
Derin Serbetcioglu <serbetd@gmail.com>
2021-04-09 19:23:08 -0700

    testing through example

--- portal/manage.py
+++ portal/manage.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 import os
 import sys
+import environ
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings.base")
 
     from django.core.management import execute_from_command_line
-    print("env", os.environ.get('DATABASE_PASSWORD'))
+    env = environ.Env()
+    print("env", env('DATABASE_PASSWORD'))
     execute_from_command_line(sys.argv)
