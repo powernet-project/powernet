06e3554c8322d2ccdbfa466817494fa5a2fef780
Derin Serbetcioglu <serbetd@gmail.com>
2021-04-12 11:13:47 -0700

    egauge print test

--- portal/manage.py
+++ portal/manage.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
     env = environ.Env()
-    print("env", env('DATABASE_PASSWORD'))
+    # print("env", env('DATABASE_PASSWORD'))
     print("how", EGAUGE_USER)
     execute_from_command_line(sys.argv)
