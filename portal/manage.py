bb39ca6dedc33953d7261c7537721603e41005a9
Derin Serbetcioglu <serbetd@gmail.com>
2021-04-12 11:21:30 -0700

    egauge print test

--- portal/manage.py
+++ portal/manage.py
@@ -2,7 +2,7 @@
 import os
 import sys
 import environ
-from settings.base import *
+from app.settings.base import *
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings.base")
 
