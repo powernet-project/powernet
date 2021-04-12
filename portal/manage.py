64ef38a342204c981e7b1adf5b04bb1ab7fa7853
Derin Serbetcioglu <serbetd@gmail.com>
2021-04-12 11:13:39 -0700

    egauge print test

--- portal/manage.py
+++ portal/manage.py
@@ -2,11 +2,12 @@
 import os
 import sys
 import environ
-
+import * from settings.base
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings.base")
 
     from django.core.management import execute_from_command_line
     env = environ.Env()
     print("env", env('DATABASE_PASSWORD'))
+    print("how", EGAUGE_USER)
     execute_from_command_line(sys.argv)
