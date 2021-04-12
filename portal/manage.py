3434fe3bf9ec0078a141bb79fea05db08a168477
Derin Serbetcioglu <serbetd@gmail.com>
2021-04-12 15:33:11 -0700

    new test

--- portal/manage.py
+++ portal/manage.py
@@ -6,7 +6,7 @@ import environ
 if __name__ == "__main__":
     env = environ.Env()
     print("yooo")
-    print("env", env("EG_PW"))
+    print("env", env("DB_PASSWORD"))
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings.gains_debug")
 
     from django.core.management import execute_from_command_line
