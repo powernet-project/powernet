e2c96605201bcbce8190e3ae8f82068f2de4e17c
Derin Serbetcioglu <serbetd@gmail.com>
2021-04-12 14:44:03 -0700

    print all

--- portal/manage.py
+++ portal/manage.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     env = environ.Env()
-    print("env", env('EG_PW'))
+    print("env", env())
     execute_from_command_line(sys.argv)
