45d2bab461219f79c800b09de3f997d0a14aba49
Derin Serbetcioglu <serbetd@gmail.com>
2021-04-12 21:04:23 -0700

    final - secret env works

--- portal/app/settings/gains_debug.py
+++ portal/app/settings/gains_debug.py
@@ -5,8 +5,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'powernet_local',
-        'USER': 'derins',
-        'PASSWORD': '',
+        'USER': 'postgres',
+        'PASSWORD': '1qaz@WSX3e',
         'HOST': 'localhost',
         'PORT': '5432'
     }
